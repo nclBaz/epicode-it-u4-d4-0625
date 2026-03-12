13a87ea3c7e4ae1e148542e4aab083de5ec55efc
Riccardo Gulin <riccardo.gulin@gmail.com>
2026-03-12 11:09:30 +0100

    Adds Override & Overload examples

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import entities.Animal;
+import entities.Cat;
+import entities.Dog;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		Animal a = new Animal("Gianni", 40);
+//		a.sayYourName();
+
+		Cat c1 = new Cat("Tom", 10, false);
+		Dog d1 = new Dog("Rex", 5, true);
+		c1.setAge(11);
+		c1.sayYourName();
+		c1.sayYourName("Ciao a tutti");
+//		d1.sayYourName();
+
+		System.out.println(c1);
+		System.out.println(d1);
+
+
 	}
 }
--- src/entities/Cat.java
+++ src/entities/Cat.java
@@ -8,4 +8,30 @@ public class Cat extends Animal {
 		super(name, age); // <-- Richiama il costruttore del padre
 		this.hasBoots = hasBoots;
 	}
+
+	public boolean hasBoots() {
+		return hasBoots;
+	}
+
+	public void setHasBoots(boolean hasBoots) {
+		this.hasBoots = hasBoots;
+	}
+
+	@Override
+	public void sayYourName() {
+		super.sayYourName();
+		System.out.println("Per essere più precisi non sono un Animal ma un Cat");
+	}
+
+	public void sayYourName(String saluto) { // OVERLOAD
+		System.out.println(saluto);
+		this.sayYourName();
+	}
+
+	@Override
+	public String toString() {
+		return "Cat{" +
+				"hasBoots=" + hasBoots +
+				"} " + super.toString();
+	}
 }
