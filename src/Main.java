a0a1ca3138a593774ee233a88e47e5e4f755fd45
Riccardo Gulin <riccardo.gulin@gmail.com>
2026-03-12 15:02:22 +0100

    Adds Polymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import entities.Animal;
 import entities.Cat;
 import entities.Dog;
+import entities.Student;
+import interfaces.Jumper;
+import interfaces.Runner;
 
 public class Main {
 	public static void main(String[] args) {
@@ -16,6 +20,44 @@ public class Main {
 		System.out.println(c1);
 		System.out.println(d1);
 
+		Student aldo = new Student("Aldo", "Baglio");
+
+		Runner[] runners = {d1, aldo, new Dog("Ringhio", 2, false)};
+		Jumper[] jumpers = {c1, aldo};
+
+		Animal[] animals = {c1, d1};
+
+//		for (Runner runner : runners) {
+//			runner.run();
+//		}
+
+		for (Jumper jumper : jumpers) {
+			jumper.jump(10);
+			if (jumper instanceof Cat) {
+				Cat c = (Cat) jumper; // Avendo verificato che è un Cat posso fare il casting a Cat ed usare i suoi metodi
+				c.makeSound();
+			} else {
+				System.out.println(jumper + " non è un gatto!");
+			}
+
+		}
+
+//		for (Animal animal : animals) {
+//			animal.makeSound();
+//		}
+//
+//		makeAnimalSound(c1);
+//		makeAnimalSound(d1);
 
 	}
+
+	public static void makeAnimalSound(Animal animal) { // Questo è un metodo con un parametro polimorfico, cioè posso passare qualsiasi tipo di Animal
+		// indifferente se siano Cat o Dog. Così facendo il metodo è molto versatile
+		animal.makeSound();
+	}
+
+//	public static void makeAnimalSound(Dog dog) { // Questo metodo accetta solo esclusivamente Dog. Non posso passargli altri tipi di Animal
+//		// quindi è un metodo meno versatile
+//		dog.makeSound();
+//	}
 }
--- src/entities/Student.java
+++ src/entities/Student.java
@@ -3,6 +3,8 @@ package entities;
 import interfaces.Jumper;
 import interfaces.Runner;
 
+import java.util.Objects;
+
 public class Student implements Jumper, Runner {
 	private String name;
 	private String surname;
@@ -37,4 +39,12 @@ public class Student implements Jumper, Runner {
 	public void run() {
 		System.out.println("Corro per scappare dagli esami");
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) return false;
+		Student student = (Student) o;
+		return Objects.equals(name, student.name) && Objects.equals(surname, student.surname);
+	}
+
 }
